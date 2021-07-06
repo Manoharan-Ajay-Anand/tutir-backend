25f2e37000868b664f7d649cfcf74e25fea5a62d
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2021-07-06 21:16:32 +0800

    upload image optimization

--- src/media/media.service.ts
+++ src/media/media.service.ts
@@ -1,20 +1,42 @@
 import { Storage } from '@google-cloud/storage';
 import { Injectable } from '@nestjs/common';
 import { env } from 'process';
+import fs = require('fs');
+import sharp = require('sharp');
+import { Stream } from 'stream';
 
 const storage = new Storage();
 
 @Injectable()
 export class MediaService {
-  async uploadFile(
-    file: Express.Multer.File,
-    category: string,
-  ): Promise<string> {
+  uploadFile(file: Express.Multer.File, category: string): Promise<string> {
     const dest = `${category}/${file.filename}`;
-    await storage.bucket(env.BUCKET_NAME).upload(file.path, {
-      destination: dest,
-      contentType: file.mimetype,
+    const fileObj = storage.bucket(env.BUCKET_NAME).file(dest);
+    let stream: Stream = fs.createReadStream(file.path);
+    if (file.mimetype.startsWith('image')) {
+      stream = stream.pipe(
+        sharp().resize(480, 360, {
+          fit: sharp.fit.inside,
+          withoutEnlargement: true,
+        }),
+      );
+    }
+    return new Promise((resolve, reject) => {
+      stream
+        .pipe(
+          fileObj.createWriteStream({
+            metadata: {
+              contentType: file.mimetype,
+              cacheControl: 'public, max-age=604800, immutable',
+            },
+          }),
+        )
+        .on('error', () => {
+          reject('Unable to upload');
+        })
+        .on('finish', () => {
+          resolve((env.STORAGE_URL || '/') + dest);
+        });
     });
-    return (env.STORAGE_URL || '/') + dest;
   }
 }
